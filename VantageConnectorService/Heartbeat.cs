2e278142ade97d9e221b15a8e61c29853183874f
Usman Kabir <usman.kabir@netwrix.com>
2024-05-30 03:14:28 +0500

    Hearbeat Demo Service is installing and starting

---
+++ VantageConnectorService/Heartbeat.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace VantageConnectorService
+{
+    internal class Heartbeat
+    {
+        private readonly System.Timers.Timer _timer;
+
+        public Heartbeat()
+        {
+            _timer = new System.Timers.Timer(1000) { AutoReset = true };
+            _timer.Elapsed += TimerElapsed;
+        }
+
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            string[] lines = new string[] { DateTime.Now.ToString() };
+            System.IO.File.AppendAllLines(@"C:\TempUsman\abc.txt", lines);
+        }
+        public void Start()
+        {
+            _timer.Start();
+        }
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+    }
+}
---
+++ VantageConnectorService/Program.cs
@@ -0,0 +1,22 @@
+﻿using Topshelf;
+using VantageConnectorService;
+
+var exitCode = HostFactory.Run(x =>
+{
+    x.Service<Heartbeat>(s =>
+    {
+        s.ConstructUsing(heartbeat => new Heartbeat());
+        s.WhenStarted(Heartbeat => Heartbeat.Start());
+        s.WhenStopped(Heartbeat => Heartbeat.Stop());
+    });
+
+    x.RunAsLocalSystem();
+
+    x.SetServiceName("Heartbeat");
+    x.SetDisplayName("Heartbeat a");
+    x.SetDescription("A joke service that periodically logs nerdy humor.");
+
+});
+
+int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+Environment.ExitCode = exitCodeValue;
